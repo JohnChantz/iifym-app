25c7a8fd6cdec8a7e78d9e60ef727eeb2934df36
Giannis Chantz <comrade.9510@gmail.com>
2018-03-27 16:24:28 +0300

    changes vol1
    Home Page:  -BMRcalculator
                         -TDEE calculator

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {IonicPage, NavController, ToastController} from 'ionic-angular';
 import {SettingsPage} from "../settings/settings";
 
 @IonicPage()
@@ -8,17 +8,71 @@ import {SettingsPage} from "../settings/settings";
   templateUrl: 'home.html',
 })
 export class HomePage {
+  private weight: number;
+  private height: number;
+  private age: number;
+  private gender: string;
+  private calories: number;
+  private caloriesCalculated: boolean = false;
+  private boxChecked: boolean = false;
+  private multiplier: number;
+  private tdee: number;
 
-  private macros:any = ['protein: 40%','carbs: 40%','fat: 20%'];
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public toastCtrl: ToastController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad HomePage');
   }
 
-  loadSettingsPage(){
+  loadSettingsPage() {
     this.navCtrl.push(SettingsPage);
   }
 
+  checkValuesNotEmpty() {
+    if (this.weight != null && this.height != null && this.age != null && this.gender != null) {
+      return true
+    } else {
+      return false;
+    }
+  }
+
+  calorieCalculator() {
+    console.log(this.weight + " " + this.height + " " + this.age + " " + this.gender);
+    let check = this.checkValuesNotEmpty();
+    if (check) {
+      if (this.gender == 'male') {
+        console.log('Calculating for Male');
+        this.calories = (10 * this.weight) + (6.25 * this.height) + (5 * this.age) + 5;
+      } else if (this.gender == 'female') {
+        console.log('Calculating for Female');
+        this.calories = (10 * this.weight) + (6.25 * this.height) - (5 * this.age) - 161;
+      }
+      this.caloriesCalculated = true;
+    } else {
+      this.showToastWithCloseButton();
+
+    }
+  }
+
+  showToastWithCloseButton() {
+    const toast = this.toastCtrl.create({
+      message: 'Please make sure you have fill in all the values!',
+      showCloseButton: true,
+      closeButtonText: 'Close'
+    });
+    toast.present();
+  }
+
+  changeCheckBox() {
+    console.log(this.boxChecked);
+    if (this.boxChecked == false)
+      this.boxChecked = true;
+    else
+      this.boxChecked = false;
+    console.log(this.boxChecked);
+  }
+  tdeeCalculator(){
+    this.tdee = this.calories * this.multiplier;
+  }
 }
